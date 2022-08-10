1dbce0e265c047eb4605b06e4f8ba5c92d37844e
jm.gaubier <jm.gaubier@gmail.com>
2022-08-10 18:31:32 +0100

    Introduction to Javascript

---
+++ concatenate.js
@@ -0,0 +1,1 @@
+console.log("hey" + "rina" + 6)
---
+++ variable-types.js
@@ -0,0 +1,6 @@
+const firstName = "Rina";
+const lastName = "Gaubier";
+let age = "30";
+let favouriteColour = "purple";
+
+console.log (firstName + " " + lastName + " " + age + " " + favouriteColour)
